46bc7c6d35a6ede4b9ec9259856b2cd5753df63e
Mohammed gaffer <the-100joker@live.com>
2023-10-27 01:57:27 +0300

    ...

---
+++ HMS.UI/obj/Release/net6.0/PubTmp/Out/wwwroot/assets/js/check-code.js
@@ -0,0 +1,41 @@
+﻿var container = document.getElementsByClassName("inputs-contianer")[0];
+//console.log("test co", container);
+
+container.onkeyup = function (e) {
+    var target = e.srcElement || e.target;
+    console.log("test target", target);
+    if (target.id === 'Input_FourthNumber' && document.getElementById("Input_FourthNumber").value !== '') {
+        $("#verifyCodeForm").submit();
+    }
+
+    var maxLength = 1;
+    var trargetLength = target.value.length;
+    //console.log("test trargetLength ", trargetLength);
+
+    if (trargetLength >= maxLength) {
+        //console.log("if test trargetLength", trargetLength);
+
+        var next = target;
+        while (next = next.nextElementSibling) {
+            if (next == null)
+                break;
+
+            if (next.tagName.toLowerCase() === "input") {
+                console.log("if test next", trargetLength);
+
+                next.focus();
+                break;
+            }
+        }
+    } else if (trargetLength === 0) {
+        var previous = target;
+        while (previous = previous.previousElementSibling) {
+            if (previous == null)
+                break;
+            if (previous.tagName.toLowerCase() === "input") {
+                previous.focus();
+                break;
+            }
+        }
+    }
+}
---
+++ HMS.UI/obj/Release/net6.0/PubTmp/Out/wwwroot/assets/sweetalert/messagebox.js
@@ -0,0 +1,67 @@
+﻿class messagebox {
+
+    static swall(message,type) {
+
+        if (type=="success") {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-start',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            });
+
+            Toast.fire({
+                icon: 'success',
+                title: message,
+            });
+        }
+
+
+        if (type == "warning") {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-start',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            });
+
+            Toast.fire({
+                icon: 'warning',
+                title: message,
+            });
+        }
+
+        if (type == "error") {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-start',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            });
+
+            Toast.fire({
+                icon: 'error',
+                title: message,
+            });
+        }
+
+
+       
+    }
+
+}
